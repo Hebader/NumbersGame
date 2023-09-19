aaf8eb460e169d026d4179d7558fbcd77cc3bac1
Heba Derawi <heba.derawi95@hotmail.com>
2023-09-19 15:52:28 +0200

    ä

--- Program.cs
+++ Program.cs
@@ -1,15 +1,29 @@
-﻿namespace NumbersGame
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace NumbersGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+           
+
+            CheckGuess();
+
+
+
+
+        }
+
+           
+            static void CheckGuess()
+        { 
+     
+
             Random randomerare = new Random(); // skapar en varibel för att slunmpa tal.
             int slump_tal = randomerare.Next(1, 21); //Slumpar ett tal mellan 1-20.
-          
 
-          
             for (int i = 0; i < 5; i++) // En for loop som kör från 0-4, dvs 5 gånger.
             {
                 int AnändareTal = int.Parse(Console.ReadLine()); //Användaren skriver in en siffra
@@ -37,15 +51,9 @@
 
                 {
                     Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök.");
-                    
+
                 }
-
-
-
             }
-
-           
-
 
 
 
