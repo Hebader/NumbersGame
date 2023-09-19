9ba4e8cdc456e38d8f82b6a8b976bad6ea5b6572
Heba Derawi <heba.derawi95@hotmail.com>
2023-09-19 15:36:29 +0200

    ä

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
             Random randomerare = new Random(); // skapar en varibel för att slunmpa tal.
             int slump_tal = randomerare.Next(1, 21); //Slumpar ett tal mellan 1-20.
-            int AnändareTal = int.Parse(Console.ReadLine()); //Användaren skriver in en siffra
+          
 
             // En for loop som låter användaren gissa 5 gånger.
-            for (int i = 1; i < 5; i++) // Börjar loopen från 1 för att användaren redan gissat en gång
+            for (int i = 0; i < 5; i++) // Börjar loopen från 1 för att användaren redan gissat en gång
             {
-               
+                int AnändareTal = int.Parse(Console.ReadLine()); //Användaren skriver in en siffra
 
                 if (slump_tal < AnändareTal)
 
@@ -33,22 +33,28 @@
                     break;
                 }
 
-                AnändareTal = int.Parse(Console.ReadLine());
+                if (i == 4)
+
+                {
+                    Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök.");
+                    break;
+                }
+
+
 
             }
 
-            while (AnändareTal != slump_tal) 
-            {
-                Console.WriteLine( "Tyvärr, du lyckades inte gissa talet på fem försök.");
-                break;
-            }
-                  
-                    
-               
-                        
-                
-            
+           
+
+
+
+
+
+
+
+
+
         }
-        static CheckGuess();
+
     }
 }
