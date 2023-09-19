20cf88f5aab2f091f9e20ec1ad84a49ff0072de1
Heba Derawi <heba.derawi95@hotmail.com>
2023-09-19 14:32:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            Random randomerare = new Random(); // skapar en varibel för att slunmpa tal.
+            int slump_tal = randomerare.Next(1, 21); //Slumpar ett tal mellan 1-20.
+            int AnändareTal = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i <= 5; i++) 
+            {
+
+                if ( slump_tal < AnändareTal)
+                   
+                { Console.WriteLine("Tyvärr, du gissade för lågt!"); 
+                }
+                if (slump_tal > AnändareTal)
+
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                }
+                if (slump_tal == AnändareTal)
+                {
+                    Console.WriteLine("Wohoo! Du klarade det!");
+                }                
+                
+            }
+        }
+    }
+}
